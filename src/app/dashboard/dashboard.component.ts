e7d2363b69160379c5c0aaae017c0a87e7f2deca
Pau Torres <CE9@costaisa.org>
2017-05-16 16:16:22 +0200

    added job filter

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,6 +21,8 @@ export class DashboardComponent {
     public items: any;
     public gnomeprofessions: any;
     public jobSelected: GnomesModel[] = [];
+    public newArray: any[] = [];
+    public bigFile: any;
 
     constructor(public fb: FormBuilder, private homeHttp: HomeService) {
         this.GnomesForm = this.fb.group({
@@ -45,29 +47,34 @@ export class DashboardComponent {
         newgnomes.weight = this.weight || null;
         newgnomes.professions_length = this.professions_length || null;
         newgnomes.jobs = this.jobSelected || null;
-        console.log(newgnomes)
+        console.log(newgnomes);
         this.items = this.homeHttp.getSelectedGnomes(newgnomes);
+        this.bigFile = this.items;
     }
 
     saveJob(job) {
 
         if (!this.jobSelected.length) {
             this.jobSelected.push(job);
+            this.items = this.bigFile
         } else {
             let index: number = this.jobSelected.indexOf(job);
             if (this.jobSelected[index] != null) this.jobSelected.splice(index, 1);
             else this.jobSelected.push(job);
-        }
-        console.log(this.jobSelected);
-        for (let u = 0; u <= this.items.length; u++) {
-            console.log(this.items[u].professions)
-            for (let b = 0; b <= this.items[u].professions.length; b++) {
-                if (this.items[u].professions[b] == job) {
-                    this.items = this.items[u].professions[b];
-                } else {
-                    console.log('nope')
+
+            for (let u = 0; u <= this.items.length - 1; u++) {
+                console.log(this.items[u].professions)
+                for (let b = 0; b <= this.professions_length - 1; b++) {
+                    if (this.items[u].professions[b] == job) {
+                        this.newArray.push(this.items[u])
+
+                        this.items = this.newArray;
+
+
+                    }
                 }
             }
         }
+
     }
 }
